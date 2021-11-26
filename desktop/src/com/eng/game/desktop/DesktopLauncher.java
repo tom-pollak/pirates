6be2c90ea8163f83fd2841c314a165cfebba84d9
Tom Pollak <tompollak1000@gmail.com>
2021-11-26 16:46:06 +0000

    initial build 2

---
+++ desktop/src/com/eng/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.eng.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.eng.game.PirateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PirateGame(), config);
+	}
+}
