ae79fd103e3481aa07551e703abf587f5c29fa70
Tom Pollak <tompollak1000@gmail.com>
2022-01-21 16:33:31 +0000

    Added a player with collision detection, movement & camera following

---
+++ TODO.md
@@ -0,0 +1,18 @@
+# TODO
+
+- [ ] Finish map
+- [ ] Create enemy ships
+    - [ ] Spawn periodically at enemy colleges
+    - [ ] Pathfind towards player and other enemies
+    - [ ] Defend college from enemies
+- [ ] Create colleges
+    - [ ] Have a health bar
+    - [ ] Can be captured when base is destroyed
+    - [ ] Have cannons that have health and shoot at players
+- [ ] Create coin/point system
+    - [ ] Holding base over time
+    - [ ] Finding random treasure chests in the ocean
+- [ ] Objectives
+    - [ ] Rescue princess
+    - [ ] Defeat main college
+    - [ ] Find the treasure chests
--- core/src/com/eng/game/screens/Play.java
+++ core/src/com/eng/game/screens/Play.java
@@ -4,37 +4,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.eng.game.entities.Player;
 
 public class Play implements Screen {
 
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
     private OrthographicCamera camera;
+    private Player player;
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        camera.position.set(player.getX() + player.getWidth() / 2, player.getY() + player.getHeight() / 2, 0);
+        camera.update();
+
         renderer.setView(camera);
         renderer.render();
-    }
 
-
-    @Override
-    public void dispose() {
-        map.dispose();
-        renderer.dispose();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        camera.viewportWidth = width;
-        camera.viewportHeight = height;
-        camera.update();
+        renderer.getBatch().begin();
+        player.draw(renderer.getBatch());
+        renderer.getBatch().end();
     }
 
     @Override
@@ -42,6 +40,24 @@ public class Play implements Screen {
         map = new TmxMapLoader().load("maps/world.tmx");
         renderer = new OrthogonalTiledMapRenderer(map);
         camera = new OrthographicCamera();
+
+        player = new Player(new Sprite(new Texture("img/player.png")), (TiledMapTileLayer) map.getLayers().get(0));
+        player.setPosition(5 * player.getCollisionLayer().getTileWidth(), 20 * player.getCollisionLayer().getTileHeight());
+
+        Gdx.input.setInputProcessor(player); // Set input to the player
+    }
+
+    @Override
+    public void dispose() {
+        map.dispose();
+        renderer.dispose();
+        player.getTexture().dispose();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        camera.viewportWidth = width / 5f;
+        camera.viewportHeight = height / 5f;
     }
 
     @Override
@@ -54,6 +70,7 @@ public class Play implements Screen {
 
     @Override
     public void hide() {
+        dispose();
 
     }
 
