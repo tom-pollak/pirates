71ba55eacee83fad844678857176fbf56c74c8f8
Tom Pollak <tompollak1000@gmail.com>
2022-01-21 16:43:16 +0000

    Moved ship logic (veloctiy & collision detection) to #Ship(), #Player now extends #Ship

--- core/src/com/eng/game/entities/Player.java
+++ core/src/com/eng/game/entities/Player.java
@@ -1,128 +1,13 @@
 package com.eng.game.entities;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
-import com.badlogic.gdx.math.Vector2;
 
-public class Player extends Sprite implements InputProcessor {
-    private float speed = 100, increment;
-    private Vector2 velocity = new Vector2();
-    private TiledMapTileLayer collisionLayer;
-
+public class Player extends Ship implements InputProcessor {
     public Player(Sprite sprite, TiledMapTileLayer collisionLayer) {
-        super(sprite);
-        this.collisionLayer = collisionLayer;
-        setSize((float) (getWidth() * 0.5), (float) (getHeight() * 0.5));
-    }
-
-    @Override
-    public void draw(Batch batch) {
-        update(Gdx.graphics.getDeltaTime());
-        super.draw(batch);
-    }
-
-    public void update(float delta) {
-
-        float oldX = getX(), oldY = getY();
-        boolean collisionX = false, collisionY = false;
-
-        setX(getX() + velocity.x * delta);
-
-        // calculate the increment for collidesLeft and collidesRight
-        increment = collisionLayer.getTileWidth();
-        increment = getWidth() < increment ? getWidth() / 2 : increment / 2;
-
-        if (velocity.x < 0) // going left
-            collisionX = collidesLeft();
-        else if (velocity.x > 0) // going right
-            collisionX = collidesRight();
-
-        // react to x collision
-        if (collisionX) {
-            setX(oldX);
-            velocity.x = 0;
-        }
-
-        // move on y
-        setY(getY() + velocity.y * delta);
-
-        // calculate the increment for collidesBottom and collidesTop
-        increment = collisionLayer.getTileHeight();
-        increment = getHeight() < increment ? getHeight() / 2 : increment / 2;
-
-        if (velocity.y < 0) // going down
-            collisionY = collidesBottom();
-        else if (velocity.y > 0) // going up
-            collisionY = collidesTop();
-
-        // react to y collision
-        if (collisionY) {
-            setY(oldY);
-            velocity.y = 0;
-        }
-    }
-
-    private boolean isCellBlocked(float x, float y) {
-        Cell cell = collisionLayer.getCell((int) (x / collisionLayer.getTileWidth()), (int) (y / collisionLayer.getTileHeight()));
-        return cell != null && cell.getTile() != null && cell.getTile().getProperties().containsKey("blocked");
-    }
-
-    public boolean collidesRight() {
-        for (float i = 0; i <= getHeight(); i += increment)
-            if (isCellBlocked(getX() + getWidth(), getY() + i))
-                return true;
-        return false;
-    }
-
-    public boolean collidesLeft() {
-        for (float i = 0; i <= getHeight(); i += increment)
-            if (isCellBlocked(getX(), getY() + i))
-                return true;
-        return false;
-    }
-
-    public boolean collidesTop() {
-        for (float i = 0; i <= getWidth(); i += increment)
-            if (isCellBlocked(getX() + i, getY() + getHeight()))
-                return true;
-        return false;
-
-    }
-
-    public boolean collidesBottom() {
-        for (float i = 0; i <= getWidth(); i += increment)
-            if (isCellBlocked(getX() + i, getY()))
-                return true;
-        return false;
-    }
-
-    public float getSpeed() {
-        return speed;
-    }
-
-    public void setSpeed(float speed) {
-        this.speed = speed;
-    }
-
-    public Vector2 getVelocity() {
-        return velocity;
-    }
-
-    public void setVelocity(Vector2 velocity) {
-        this.velocity = velocity;
-    }
-
-    public TiledMapTileLayer getCollisionLayer() {
-        return collisionLayer;
-    }
-
-    public void setCollisionLayer(TiledMapTileLayer collisionLayer) {
-        this.collisionLayer = collisionLayer;
+        super(sprite, collisionLayer);
     }
 
     @Override
